ba0b6ed56af3f5bd21257c57d218bd81603f5f7e
hodrag <31184354+hodrag@users.noreply.github.com>
2023-02-18 21:03:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,22 +6,22 @@ echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf
 #echo 'src-git Rinze https://github.com/Lancet-z/luci-theme-Rinze.git' >>feeds.conf.default
 
 #istore
-echo 'src-git istore https://github.com/linkease/istore' >> feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-echo 'src-git packages https://github.com/xiaoqingfengATGH/feeds-packages' >> feeds.conf.default
-echo 'src-git luci https://github.com/xiaoqingfengATGH/feeds-luci' >> feeds.conf.default
-echo 'src-git routing https://github.com/coolsnowwolf/routing' >> feeds.conf.default
-echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >> feeds.conf.default
-echo 'src-git pwluci https://github.com/homelede/openwrt-passwall.git;luci' >> feeds.conf.default
-echo 'src-git pw https://github.com/homelede/openwrt-passwall.git;packages' >> feeds.conf.default
-echo 'src-git xiaoqingfeng https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng' >> feeds.conf.default
-echo 'src-git lienol https://github.com/homelede/openwrt-package.git' >> feeds.conf.default
+echo 'src-git packages https://github.com/xiaoqingfengATGH/feeds-packages' >>feeds.conf.default
+echo 'src-git luci https://github.com/xiaoqingfengATGH/feeds-luci' >>feeds.conf.default
+echo 'src-git routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
+echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
+echo 'src-git pwluci https://github.com/homelede/openwrt-passwall.git;luci' >>feeds.conf.default
+echo 'src-git pw https://github.com/homelede/openwrt-passwall.git' >>feeds.conf.default
+echo 'src-git xiaoqingfeng https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng' >>feeds.conf.default
+echo 'src-git lienol https://github.com/homelede/openwrt-package.git;main' >>feeds.conf.default
 # luci-theme-infinityfreedom
-echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >> feeds.conf.default
+echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
 # strongswanInDocker
-echo 'src-git strongswanInDocker https://github.com/xiaoqingfengATGH/luci-app-strongswanInDocker.git' >> feeds.conf.default
-echo 'src-git alist https://github.com/alist-org/alist.git' >> feeds.conf.default
-echo 'src-git dockerman https://github.com/bixuehujin/dockerman.git' >> feeds.conf.default
+echo 'src-git strongswanInDocker https://github.com/xiaoqingfengATGH/luci-app-strongswanInDocker.git' >>feeds.conf.default
+echo 'src-git alist https://github.com/alist-org/alist.git' >>feeds.conf.default
+echo 'src-git dockerman https://github.com/bixuehujin/dockerman.git' >>feeds.conf.default
