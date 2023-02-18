c31002138e09375ab1b5ecf84ba89d0e5a5f5553
hodrag <31184354+hodrag@users.noreply.github.com>
2023-02-18 20:08:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,29 @@
-#!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
+#新的argon主题
+#rm -rf ./package/lean/luci-theme-argon
+#openclash
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
+#主题列表
+#echo 'src-git Rinze https://github.com/Lancet-z/luci-theme-Rinze.git' >>feeds.conf.default
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#istore
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+src-git packages https://github.com/xiaoqingfengATGH/feeds-packages
+src-git luci https://github.com/xiaoqingfengATGH/feeds-luci
+src-git routing https://github.com/coolsnowwolf/routing
+src-git telephony https://git.openwrt.org/feed/telephony.git
+src-git pwluci https://github.com/homelede/openwrt-passwall.git;luci
+src-git pw https://github.com/homelede/openwrt-passwall.git;packages
+src-git xiaoqingfeng https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng
+src-git lienol https://github.com/homelede/openwrt-package.git;main
+# luci-theme-infinityfreedom
+src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git
+# homeclash
+src-git homeclash https://github.com/xiaoqingfengATGH/homeclash.git
+# strongswanInDocker
+src-git strongswanInDocker https://github.com/xiaoqingfengATGH/luci-app-strongswanInDocker.git
+src-git alist https://github.com/alist-org/alist.git
+src-git dockerman https://github.com/bixuehujin/dockerman.git
